c26d9a0882ccc2c11e4a6953d29e80b8523aae7e
ewheeler <evanmwheeler@gmail.com>
2009-12-16 10:43:10 +0000

    removed non WHO stuff

--- utils.py
+++ utils.py
@@ -17,10 +17,7 @@ except ImportError:
     # Python 2.6 includes json library
     import json
 
-from tables import stunting_boys, stunting_girls
-from tables import weight_for_height as weight_for_height_table
-
-class childgrowth(object):
+class ChildGrowth(object):
     # TODO extract this class into its own library
     def __init__(self, adjust_height_data = False, adjust_weight_scores = False):
         # use decimal.Decimal instead of float to avoid unwanted rounding
@@ -486,9 +483,11 @@ class childgrowth(object):
                     return zscore.quantize(D('.01'))
 
 
-class util(object):
+class helpers(object):
     @staticmethod   
     def get_good_date(date, delimiter=False):
+        # TODO parameter to choose formating
+        # e.g., DDMMYY vs YYMMDD etc
         print 'getting good date...'
         print date
         delimiters = r"[./\\-]+"
@@ -599,77 +598,3 @@ class util(object):
                 return None
         except Exception, e:
             print e 
-####
-#
-#  TODO the below functions are from andymckay's malnutrition work
-#       and the SAM/MAM stuff in childhealth/models.py (on the
-#       Assessment model) should be ported to use these functions
-#       as well as the flat data files rather than having the
-#       overhead of a HealthTables app 
-#
-####
-
-    @staticmethod
-    def years_months(date):
-        now = datetime.now().date()
-        ymonths = (now.year - date.year) * 12
-        months = ymonths + (now.month - date.month)
-        return (now.year - date.year, months)
-        
-    @staticmethod
-    def stunting(date, gender):
-        assert gender.lower() in ["m", "f"]
-        years, months = years_months(date)
-        # we have a month eg: 9, so we assume that is 8.5, since
-        # it can't be 9.5... assuming the .5's are there to make sure that
-        if int(months) > 73:
-            raise ValueError, "Stunting charts only go as high as 72.5 months"
-        elif int(months) >= 1:
-            months = str(int(months) - 0.5)
-        else:
-            # lowest bound
-            months = 0
-            
-        if gender.lower() == "m":
-            stunts = stunting_boys.data
-        else:
-            stunts = stunting_girls.data
-        return stunts[months]
-
-    @staticmethod
-    def _dumb_round(number):
-        # please improve
-        assert isinstance(number, (float, int)), "Got a %s, which is a: %s" % (number, type(number)) # forget duck typing, this won't work on anything else
-        remainder = number - int(number)
-        if remainder >= 0.5:
-            remainder = 0.5
-        else:
-            remainder = 0.0
-        return int(number) + remainder
-
-    range_strs = ["60%-", "70%-60%","75%-70%", "80%-75%","85%-80%","100%-85%"]
-
-    @staticmethod
-    def _get_range_str(value, targets):
-        targets.reverse()
-        result = "60%-"
-        for text, target in zip(range_strs, targets):
-            target = float(target)
-            if value >= target:
-                result = text
-        return result
-        
-    @staticmethod
-    def weight_for_height(height, weight):
-        weight = float(weight)
-        number = _dumb_round(height)
-        
-        if number < 49.0:
-            # raise ValueError, "Weight for height charts only go as low as 85.0, got height %s." % height
-            return None
-        elif number > 130.0:
-            # raise ValueError, "Weight for height charts only go as high as 130.0, got height %s." % height
-            return None
-            
-        targets = weight_for_height_data.data[str(number)][:]
-        return _get_range_str(weight, targets)
